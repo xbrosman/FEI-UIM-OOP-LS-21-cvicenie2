f3f16c2a68aab917ff1280b00b5af1af2e02fff5
Jakub Kovář <jakub.kovar@stuba.sk>
2021-02-22 14:35:32 +0100

    nerovnomerne 2D polia

--- src/main/java/sk/stuba/fei/uim/oop/Cvicenie2.java
+++ src/main/java/sk/stuba/fei/uim/oop/Cvicenie2.java
@@ -4,54 +4,17 @@ import java.util.Arrays;
 
 public class Cvicenie2 {
     public static void main(String[] args) {
-        int x = 5;
-        int[] y = new int[]{1, 2, 3};
-        FakeStruktura z = new FakeStruktura();
+        int[][] pole = new int[10][];
 
-        System.out.println(x);
-        pokusRaw(x);
-        System.out.println(x);
+        for (int i = 0; i < pole.length; i++) {
+            pole[i] = new int[i+1];
+            for (int j = 0; j < pole[i].length; j++) {
+                pole[i][j] = j;
+            }
+        }
 
-        System.out.println(Arrays.toString(y));
-        pokusArray1(y);
-        System.out.println(Arrays.toString(y));
-        pokusArray2(y);
-        System.out.println(Arrays.toString(y));
-
-        System.out.println(z.x);
-        z.x = 7;
-        System.out.println(z.x);
-        pokusObjekt(z);
-        System.out.println(z.x);
-
-        FakeStruktura bod = bod();
-        System.out.println(bod.x + " " + bod.y);
-    }
-
-    public static void pokusRaw(int cislo) {
-        cislo = 7;
-        System.out.println(cislo);
-    }
-
-    public static void pokusArray1(int[] array) {
-        array[0] = 10;
-        System.out.println(Arrays.toString(array));
-    }
-
-    public static void pokusArray2(int[] array) {
-        array = new int[]{4, 5, 6,};
-        System.out.println(Arrays.toString(array));
-    }
-
-    public static void pokusObjekt(FakeStruktura struktura) {
-        struktura.x = 10;
-        System.out.println(struktura.x);
-    }
-
-    public static FakeStruktura bod() {
-        FakeStruktura f = new FakeStruktura();
-        f.y = 10;
-        f.x = 10;
-        return f;
+        for (int i = 0; i < pole.length; i++) {
+            System.out.println(Arrays.toString(pole[i]));
+        }
     }
 }
