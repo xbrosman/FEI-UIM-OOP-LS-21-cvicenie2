221b43541867d812398cb2ae01809aa195de3022
Jakub Kovář <jakub.kovar@stuba.sk>
2021-02-22 14:04:26 +0100

    pointre a funkcie 2

--- src/main/java/sk/stuba/fei/uim/oop/Cvicenie2.java
+++ src/main/java/sk/stuba/fei/uim/oop/Cvicenie2.java
@@ -12,7 +12,9 @@ public class Cvicenie2 {
         System.out.println(x);
 
         System.out.println(Arrays.toString(y));
-        pokusArray(y);
+        pokusArray1(y);
+        System.out.println(Arrays.toString(y));
+        pokusArray2(y);
         System.out.println(Arrays.toString(y));
     }
 
@@ -21,8 +23,13 @@ public class Cvicenie2 {
         System.out.println(cislo);
     }
 
-    public static void pokusArray(int[] array) {
+    public static void pokusArray1(int[] array) {
         array[0] = 10;
+        System.out.println(Arrays.toString(array));
+    }
+
+    public static void pokusArray2(int[] array) {
+        array = new int[]{4, 5, 6,};
         System.out.println(Arrays.toString(array));
     }
 }
