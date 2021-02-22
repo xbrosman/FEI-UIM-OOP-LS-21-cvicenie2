9a0183cca68006884bde047a0d9c25ff84153681
Jakub Kovář <jakub.kovar@stuba.sk>
2021-02-22 13:48:46 +0100

    Arrays

--- src/main/java/sk/stuba/fei/uim/oop/Cvicenie2.java
+++ src/main/java/sk/stuba/fei/uim/oop/Cvicenie2.java
@@ -1,17 +1,14 @@
 package sk.stuba.fei.uim.oop;
 
-import java.util.Objects;
+import java.util.Arrays;
 
 public class Cvicenie2 {
     public static void main(String[] args) {
-        String a = "a";
-        String b = null;
-        String c = KeyboardInput.readString("zadajte text");
-        System.out.println("a == a" + (a == a));
-        System.out.println("a == b" + (a == b));
-        System.out.println("a == c" + (a == c));
-        System.out.println("Objects.equals(a, c)" + Objects.equals(a, c));
-        System.out.println("Objects.equals(b, c)" + Objects.equals(b, c));
-        System.out.println("a.equals(c)" + a.equals(c));
+        int[] x = new int[]{1, 2, 3};
+        int[] y = new int[3];
+        for (int i = 0; i < y.length; i++) {
+            y[i] = i + 1;
+        }
+        System.out.println(Arrays.equals(x, y));
     }
 }
