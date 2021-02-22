35dfebc0d4b0f1a8cb7b51654ed83131a80ef644
Jakub Kovář <jakub.kovar@stuba.sk>
2021-02-22 14:13:16 +0100

    pointre a objekty

--- src/main/java/sk/stuba/fei/uim/oop/Cvicenie2.java
+++ src/main/java/sk/stuba/fei/uim/oop/Cvicenie2.java
@@ -6,6 +6,7 @@ public class Cvicenie2 {
     public static void main(String[] args) {
         int x = 5;
         int[] y = new int[]{1, 2, 3};
+        FakeStruktura z = new FakeStruktura();
 
         System.out.println(x);
         pokusRaw(x);
@@ -16,6 +17,15 @@ public class Cvicenie2 {
         System.out.println(Arrays.toString(y));
         pokusArray2(y);
         System.out.println(Arrays.toString(y));
+
+        System.out.println(z.x);
+        z.x = 7;
+        System.out.println(z.x);
+        pokusObjekt(z);
+        System.out.println(z.x);
+
+        FakeStruktura bod = bod();
+        System.out.println(bod.x + " " + bod.y);
     }
 
     public static void pokusRaw(int cislo) {
@@ -31,5 +41,17 @@ public class Cvicenie2 {
     public static void pokusArray2(int[] array) {
         array = new int[]{4, 5, 6,};
         System.out.println(Arrays.toString(array));
+    }
+
+    public static void pokusObjekt(FakeStruktura struktura) {
+        struktura.x = 10;
+        System.out.println(struktura.x);
+    }
+
+    public static FakeStruktura bod() {
+        FakeStruktura f = new FakeStruktura();
+        f.y = 10;
+        f.x = 10;
+        return f;
     }
 }
