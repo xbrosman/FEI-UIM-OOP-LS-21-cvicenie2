006ff69263949180b803dea64990a5aaef965ac4
Jakub Kovář <jakub.kovar@stuba.sk>
2021-02-22 13:37:07 +0100

    porovnavanie objektov

--- src/main/java/sk/stuba/fei/uim/oop/Cvicenie2.java
+++ src/main/java/sk/stuba/fei/uim/oop/Cvicenie2.java
@@ -1,7 +1,17 @@
 package sk.stuba.fei.uim.oop;
 
+import java.util.Objects;
+
 public class Cvicenie2 {
     public static void main(String[] args) {
-
+        String a = "a";
+        String b = null;
+        String c = KeyboardInput.readString("zadajte text");
+        System.out.println("a == a" + (a == a));
+        System.out.println("a == b" + (a == b));
+        System.out.println("a == c" + (a == c));
+        System.out.println("Objects.equals(a, c)" + Objects.equals(a, c));
+        System.out.println("Objects.equals(b, c)" + Objects.equals(b, c));
+        System.out.println("a.equals(c)" + a.equals(c));
     }
 }
